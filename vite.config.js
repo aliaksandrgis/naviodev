b8627c00d678d69783a97b0baa8f2b5969451b7e
aliaksandrgis <aliaksandrgis@gmail.com>
2025-11-24 18:35:32 +0100

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+﻿import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: env.VITE_BASE_PATH || "/",
+    plugins: [vue()],
+  };
+});
