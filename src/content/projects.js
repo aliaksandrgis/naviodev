c5e5fdc05bc7b4bae87114d65bfc57de6fdf8e05
aliaksandrgis <aliaksandrgis@gmail.com>
2026-01-06 17:49:38 +0100

    add airquality-suite github page

--- src/content/projects.js
+++ src/content/projects.js
@@ -9,7 +9,7 @@
     name: "Data Pipelines Suite",
     description: "ETL/ELT pipelines on Airflow + Kafka with SLA/SLO monitoring.",
     stack: ["Airflow", "Kafka", "PostGIS", "Docker"],
-    links: { github: "https://github.com/example/data-pipelines", demo: "https://air.naviodev.com" }
+    links: { github: "https://github.com/aliaksandrgis/airquality-suite", demo: "https://air.naviodev.com" }
   },
   {
     name: "Mobile Field Toolkit",
